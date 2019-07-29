51b14334c1002f745621e141c590140cc7640145
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-07-29 02:52:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,7 +63,7 @@ class Student
     sql = <<-SQL
      SELECT *
      FROM students
-     WHERE grade = 10 ORDER BY ASC
+     WHERE grade = 10 ORDER BY grade
     SQL
     
     DB[:conn].execute(sql, name).map do |row|
