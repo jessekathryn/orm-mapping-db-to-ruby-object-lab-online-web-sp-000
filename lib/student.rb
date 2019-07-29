1d5534ce0212ad3d6b087dff9b962c94a83ed66e
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-07-29 02:38:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,7 @@ class Student
     end
   end
   
- def self.first_X_students_in_grade_10
+ def self.first_X_students_in_grade_10(name)
     sql = <<-SQL
      SELECT *
      FROM students
@@ -69,7 +69,7 @@ class Student
      LIMIT 1
     SQL
     
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
   end 
