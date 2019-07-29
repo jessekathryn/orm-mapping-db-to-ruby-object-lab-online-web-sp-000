3f0d7825b1ff8cad5cbb3c287da13952aee33b04
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-07-29 02:28:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,12 +51,14 @@ class Student
     sql = <<-SQL
      SELECT *
      FROM students
-     WHERE grade = 11
+     WHERE grade = 9, 10, 11
     SQL
     
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end.size
+      
+    sql.size
+    end
   end
   
   def save
